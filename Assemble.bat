e6466bdf9af4a9ae2dae66f4e33755abfc8e9ef9
CarlosHenrique13 <carloshenriquexx123@gmail.com>
2020-07-09 10:10:02 -0300

    Estrutura

---
+++ Assemble.bat
@@ -0,0 +1,13 @@
+echo off
+cls
+
+echo Monntando o arquivo "Bootloader"
+nasm -f bin bootloader.asm -o Binary/bootloader.bin
+
+echo Montando o arquivo "Kernei"
+nasm -f bin kernel.asm -o Binary/kernel.bin
+
+echo Montando o arquivo "Window"
+nasm -f bin window.asm -o Binary/window.bin
+
+pause
