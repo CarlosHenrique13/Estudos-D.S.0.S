5fe9b820dea9f270e0417738d04d92860860d135
CarlosHenrique13 <62728022+CarlosHenrique13@users.noreply.github.com>
2020-10-12 09:25:45 -0300

    fontswrite asm

---
+++ fontswriter.asm
@@ -0,0 +1,10 @@
+%INCLUDE "Hardware\memory.lib"
+[BITS SYSTEM]
+[ORG FONTSWRITER]
+
+
+
+%INCLUDE "Hardware\win16.lib"
+%INCLUDE "Hardware\keyboard.lib"
+%INCLUDE "Hardware\fontswriter.lib"
+%INCLUDE "Hardware\fonts.lib"
